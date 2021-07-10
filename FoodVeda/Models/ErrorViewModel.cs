3ce197fcc41654e07b882f1e8155a31d7fad56c8
Sanjok Gyawali <sanzokgyawali123@gmail.com>
2021-07-10 09:45:39 +0545

    Initial Commit

---
+++ FoodVeda/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FoodVeda.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
